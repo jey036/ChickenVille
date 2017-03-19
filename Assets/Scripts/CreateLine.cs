caf734626437797c934a460694637bc3ca1f30ff
jey036 <jey036@ucsd.edu>
2017-03-18 23:19:03 -0700

    Added some scripts :)

---
+++ Assets/Scripts/CreateLine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLine : MonoBehaviour {
+
+    public bool grabbed;
+    public GameObject bucket;
+
+    LineRenderer line;
+    GameObject[] chickens;
+
+    // Use this for initialization
+    void Start()
+    {
+        grabbed = false;
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.widthMultiplier = 0.01f;
+        line.numPositions = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!grabbed)
+        {
+            chickens = GameObject.FindGameObjectsWithTag("Chicken");
+            GameObject closest = null;
+            float distance = Mathf.Infinity;
+            Vector3 position = transform.position;
+            foreach (GameObject c in chickens)
+            {
+                Vector3 diff = c.transform.position - position;
+                float dist = diff.sqrMagnitude;
+                if (dist < distance)
+                {
+                    closest = c;
+                    distance = dist;
+                }
+            }
+
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, closest.transform.position);
+        }
+        else {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, bucket.transform.position);
+        }
+    }
+}
